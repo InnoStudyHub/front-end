21547df1ebc0694afbaa97a5d2e861876b1801d7
Makshe Seytkaliev <3meuka0@gmail.com>
2022-10-19 15:58:51 +0300

    create deck_preview model

---
+++ lib/model/models/deck_preview.dart
@@ -0,0 +1,15 @@
+class DeckPreview {
+  final String deckTitle;
+  final String courseName;
+  final int numberOfCards;
+  final int year;
+  final bool isFavourite;
+
+  DeckPreview({
+    required this.deckTitle,
+    required this.courseName,
+    required this.numberOfCards,
+    required this.year,
+    required this.isFavourite,
+  });
+}
