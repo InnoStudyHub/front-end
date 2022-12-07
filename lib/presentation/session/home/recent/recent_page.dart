06c5b07a4cc5bb8bcd335a72d5d2519750f83f23
Nurdaulet Kunkhozhaev <n.kunkhozhaev@gmail.com>
2022-12-07 21:12:23 +0300

    Web UI minor fixes

--- lib/presentation/session/home/recent/recent_page.dart
+++ lib/presentation/session/home/recent/recent_page.dart
@@ -1,8 +1,11 @@
+import 'package:dynamic_height_grid_view/dynamic_height_grid_view.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'recent_controller.dart';
-import '../../deck_preview/deck_preview.dart';
 import 'package:get/get.dart';
+
 import '../../../util/color_codes.dart';
+import '../../deck_preview/deck_preview.dart';
+import 'recent_controller.dart';
 
 class RecentPage extends StatelessWidget {
   const RecentPage({Key? key}) : super(key: key);
@@ -10,6 +13,8 @@ class RecentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Get.lazyPut<RecentController>(() => RecentController());
+
+    var screenSize = MediaQuery.of(context).size;
 
     return GetBuilder<RecentController>(builder: (controller) {
       return Scaffold(
@@ -26,16 +31,46 @@ class RecentPage extends StatelessWidget {
                       ),
                     ),
                   )
-                : ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: controller.decks.length,
-                    itemBuilder: (context, index) {
-                      return DeckPreview(deck: controller.decks[index]);
-                    },
-                  ),
+                : kIsWeb
+                    ? _webRecentPage(controller, screenSize)
+                    : ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: controller.decks.length,
+                        itemBuilder: (context, index) {
+                          return DeckPreview(deck: controller.decks[index]);
+                        },
+                      ),
           ),
         ),
       );
     });
+  }
+
+  _webRecentPage(RecentController controller, Size screenSize) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(108, 20, 102, 5),
+        child: DynamicHeightGridView(
+          shrinkWrap: true,
+          physics: const BouncingScrollPhysics(),
+          /*gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      childAspectRatio:
+                      screenSize.width / screenSize.height * 1.4,
+                    ),*/
+          crossAxisCount: screenSize.width > 1500
+              ? 4
+              : screenSize.width > 1300
+                  ? 3
+                  : screenSize.width > 1000
+                      ? 2
+                      : 1,
+          itemCount: controller.decks.length,
+          builder: (context, index) {
+            return DeckPreview(deck: controller.decks[index]);
+          },
+        ),
+      ),
+    );
   }
 }
