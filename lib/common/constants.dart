44f08b495b3564a72df1ee5e262e41aa768ff638
seytkalievm <3meuka0@gmail.com>
2023-05-14 22:49:35 +0300

    hardcoded url&API key

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,4 +1,4 @@
-const String serverIP = String.fromEnvironment("SERVER_ENDPOINT");
+const String serverIP = "http://api.studyhub.kz";
 const int registerSuccessCode = 201;
 const int registerCredentialsAlreadyExistCode = 400;
 const int loginSuccessCode = 200;
@@ -6,4 +6,4 @@ const int loginWrongCredentialsCode = 401;
 const String authorizationUri =
     "https://sso.university.innopolis.ru/adfs/oauth2/authorize/?response_type=code&client_id=02bdd68b-3508-40fa-aa30-c2b9e6f2f4c5&redirect_uri=https%3A%2F%2Fstudyhub.kz%2Foauth2%2Fcallback.html";
 const String redirectUri = "https://studyhub.kz/oauth2/callback.html";
-const String apiKey = String.fromEnvironment("API_KEY");
+const String apiKey = "fi8ibhj0.7KRSXR0SsplGzXHG8y3qmKK8U7DVmKMm";
