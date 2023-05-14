c21158684df3d2591ba7658d250b69b43d7d561f
Nurdaulet Kunkhozhaev <n.kunkhozhaev@gmail.com>
2023-05-14 16:35:08 +0300

    Working on getting decks on Home Page

--- lib/presentation/session/home/home_controller.dart
+++ lib/presentation/session/home/home_controller.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../../model/repository/deck_repository.dart';
+import '../../../model/models/deck.dart';
+import '../../util/color_codes.dart';
 
 class HomeController extends GetxController
     with GetSingleTickerProviderStateMixin {
   late TabController tabController;
+  DeckRepository deckRepo = Get.find<DeckRepository>();
+
+  RxList<Deck> decks = RxList();
+
+  bool hasResults = false;
+
+  void getDecks() async {
+    showLoadingIndicator();
+    var response = await deckRepo.getDecks();
+    Navigator.of(Get.overlayContext!).pop();
+
+    decks.value = response.data!;
+    hasResults = (decks.isNotEmpty);
+    update();
+  }
+
+  void showLoadingIndicator() {
+    Get.dialog(
+      const AlertDialog(
+        backgroundColor: mainAppColor,
+        content: SizedBox(
+          height: 50,
+          child: Center(
+            child: CircularProgressIndicator(color: selectedTabColor),
+          ),
+        ),
+      ),
+      barrierDismissible: false,
+    );
+  }
 
   @override
   void onInit() {
     tabController = TabController(vsync: this, length: 3);
-
+    getDecks();
     super.onInit();
   }
 }
--- lib/presentation/session/home/home_page.dart
+++ lib/presentation/session/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../util/color_codes.dart';
+import '../deck_preview/deck_preview.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatelessWidget {
@@ -9,13 +11,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<HomeController>(builder: (controller) {
-      return const Scaffold(
-        body: Center(
-          child: Text(
-            "Здесь должен быть список дек",
-            style: TextStyle(
-              color: Colors.white,
-            ),
+      return Scaffold(
+        body: SafeArea(
+          child: Expanded(
+            child: controller.hasResults
+                ? ListView(
+                    children: [
+                      Obx(
+                        () => ListView.builder(
+                          shrinkWrap: true,
+                          physics: const BouncingScrollPhysics(),
+                          itemCount: controller.decks.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return DeckPreview(
+                              deck: controller.decks[index],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  )
+                : const Center(
+                    child: Text(
+                      "There are no decks",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: selectedMenuColor,
+                      ),
+                    ),
+                  ),
           ),
         ),
       );
